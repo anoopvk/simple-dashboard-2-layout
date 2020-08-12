e03c147581a15a0931d8e0296c2805ef36d6c28c
Anoop <anoopvk225@gmail.com>
2020-08-13 02:29:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Row, Col ,Layout, Menu, Breadcrumb } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {  Sider, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout className="layout" style={{ minHeight: '100vh' }}>
+        <Sider theme="light"></Sider>
+        <Layout>
+          <Content className="bg">
+            <Row>
+              <Col span={24}>
+              <div className="box1 box"></div>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={9}>
+              <div className="box2 box"></div>
+              </Col>
+              <Col span={15}>
+              <div className="box2 box"></div>
+              </Col>
+            </Row>
+            <Row className="mt-1">
+              <Col span={8}>
+              <div className="box3 box31 box"></div>
+              </Col>
+              <Col span={8}>
+              <div className="box3 box32 box"></div>
+              </Col>
+              <Col span={8}>
+              <div className="box3 box33 box"></div>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+        <Sider width="300" theme="light"></Sider>
+
+      </Layout>
+    );
+  }
 }
 
 export default App;
+
+
